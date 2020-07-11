a0aa20c5d2e7933a5834d39dfe064390bf1927d7
James Martinez <airtorque1@gmail.com>
2020-07-11 09:28:47 -0400

    reactivated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 10.times do
-#     Book.create(
-#       title: Faker::Book.title,
-#       author: Faker::Book.author,
-#       pubdate: Faker::Date.between(from: '1950-01-01', to: '2020-12-31'),
-#       genre: Faker::Book.genre,
-#       isbn: Faker::IDNumber.invalid
-#     )
-#   end
+    Book.create(
+      title: Faker::Book.title,
+      author: Faker::Book.author,
+      pubdate: Faker::Date.between(from: '1950-01-01', to: '2020-12-31'),
+      genre: Faker::Book.genre,
+      isbn: Faker::IDNumber.invalid
+    )
+  end
 
-# 10.times do
-#     Guest.create(
-#       name: Faker::Name.name,
-#       address: Faker::Address.full_address
-#     )
-#   end
+10.times do
+    Guest.create(
+      name: Faker::Name.name,
+      address: Faker::Address.full_address
+    )
+  end
 
-# 10.times do
-#     Checkout.create(
-#         book_id: Faker::Number.between(from: 1, to: 10),
-#         guest_id: Faker::Number.between(from: 1, to: 10),
-#         start: Faker::Date.between(from: 21.days.ago, to: Date.today),
-#         due: Faker::Date.between(from: Date.today, to: 21.days.from_now),
-#         returned: Faker::Date.between(from: 5.days.ago, to: 30.days.from_now)
-#     )
-# end
+10.times do
+    Checkout.create(
+        book_id: Faker::Number.between(from: 1, to: 10),
+        guest_id: Faker::Number.between(from: 1, to: 10),
+        start: Faker::Date.between(from: 21.days.ago, to: Date.today),
+        due: Faker::Date.between(from: Date.today, to: 21.days.from_now),
+        returned: Faker::Date.between(from: 5.days.ago, to: 30.days.from_now)
+    )
+end
